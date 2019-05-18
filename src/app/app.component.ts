d718453a7bc419295a6ec13cd937e3ed7caaf6cd
SakshiShreya <shreyasakshi96@gmail.com>
2019-05-18 16:45:06 +0530

    Added hamburger menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'OnlinePortfolio';
+export class AppComponent implements OnInit {
+  $: any;
+  els: any;
+  count: number;
+
+  ngOnInit() {
+    this.els = $('.menu a, .menu header');
+    this.count = this.els.length;
+    const groupLength = Math.ceil(this.count / 3);
+    let groupNumber = 0;
+    let i = 1;
+
+    $('.menu').css('--count', this.count + '');
+    this.els.each(function(j) {
+      if (i > groupLength) {
+        groupNumber++;
+        i = 1;
+      }
+      $(this).attr('data-group', groupNumber);
+      i++;
+    });
+
+    this.toggle();
+  }
+
+  toggle() {
+    const count = this.count;
+    this.els.each(function(j) {
+      console.log((count - j) * 0.1 + 's');
+      $(this).css('--top', $(this)[0].getBoundingClientRect().top + $(this).attr('data-group') * -15 - 20 + 'px');
+      $(this).css('--delay-in', j * 0.1 + 's');
+      $(this).css('--delay-out', (count - j) * 0.1 + 's');
+    });
+    $('.menu').toggleClass('closed');
+  }
 }
